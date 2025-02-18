9dd1ddae0e661011c195f19a597a8ef210899fab
Kovacsics Robert <kovirobi@gmail.com>
2025-02-18 01:15:45 +0000

    Add adafruit-nrftool to env

--- flake.nix
+++ flake.nix
@@ -15,8 +15,33 @@
 
     zephyr-nix = {
       url = "github:adisbladis/zephyr-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        zephyr.follows = "zephyr";
+        pyproject-nix.follows = "pyproject-nix";
+      };
+    };
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.zephyr.follows = "zephyr";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        nixpkgs.follows = "nixpkgs";
+      };
     };
   };
 
@@ -33,6 +58,40 @@
         let
           pkgs = inputs'.nixpkgs.legacyPackages;
           zephyr = inputs'.zephyr-nix.packages;
+
+          # adafruit-nrfutil-env
+          # https://github.com/FerranAD/simple-uv2nix/blob/main/flake.nix
+          workspaceRoot = ./adafruit-nrfutil-env;
+          python = pkgs.python313;
+          workspace = inputs.uv2nix.lib.workspace.loadWorkspace {
+            inherit workspaceRoot;
+          };
+          overlay = workspace.mkPyprojectOverlay {
+            sourcePreference = "wheel";
+          };
+          baseSet = pkgs.callPackage inputs.pyproject-nix.build.packages {
+            inherit python;
+          };
+          # Add overrides to base set, due to Python lock files not including
+          # build system, see https://github.com/astral-sh/uv/issues/5190
+          pythonSet = baseSet.overrideScope (
+            pkgs.lib.composeManyExtensions [
+              # A set of pre-made overrides
+              inputs.pyproject-build-systems.overlays.default
+              # uv2nix overlay
+              overlay
+              (final: prev: {
+                adafruit-nrfutil = prev.adafruit-nrfutil.overrideAttrs (old: {
+                  nativeBuildInputs = old.nativeBuildInputs ++ [
+                    (final.resolveBuildSystem {
+                      setuptools = [ ];
+                    })
+                  ];
+                });
+              })
+            ]
+          );
+          venv = pythonSet.mkVirtualEnv "venv" workspace.deps.default;
         in
         {
           devShells.default = pkgs.mkShell {
@@ -49,6 +108,7 @@
               pkgs.ninja
               pkgs.uv
               pkgs.gcc-arm-embedded
+              venv
             ];
             CMAKE_EXPORT_COMPILE_COMMANDS = "ON";
             CMAKE_GENERATOR = "Ninja";
